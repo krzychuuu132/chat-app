a49009a0b254508996fd66127e15a8cdeb7cc1e8
krzychuuu132 <52799848+krzychuuu132@users.noreply.github.com>
2020-06-28 00:25:22 +0200

    creating a join section/form validation/getting data

---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,24 @@
+import  React,{useEffect}  from 'react';
+import  querystring from 'query-string';
+import io from 'socket.io-client'
+
+const Chat = ({ location }) => {
+
+    const [ name,setName ] = useState('');
+    const [ room,setRoom ] = useState('');
+
+    useEffect(() => {
+
+        const  { name , room }  = querystring.parse(location.search);
+        
+        
+        setName(name);
+        setRoom(room);
+    })
+
+    return ( 
+        <h1>Chat!!</h1>
+     );
+}
+ 
+export default Chat;
---
+++ src/components/Join/Join.js
@@ -0,0 +1,55 @@
+import React,{ useState ,useRef } from 'react';
+import { Link ,useHistory} from 'react-router-dom';
+import gsap from 'gsap'
+
+const Join = () => {
+    const history = useHistory();
+
+    const [ name,setName ] = useState('');
+    const [ room,setRoom ] = useState('');
+    const [ error,setError ] = useState(false);
+
+    const errorRef = useRef(null);
+
+    const handleJoinClick = (e) => {
+        e.preventDefault();
+
+        if(!name || !room) {
+            setError(true);
+
+            setTimeout(()=>{
+                gsap.fromTo(errorRef.current,{opacity:0},{ duration:.8, opacity:1});
+            },.2)
+
+            
+        }
+        
+        else {
+            history.push(`/chat?name=${name}&room=${room}`);
+            setError(false);
+        }
+       
+    }
+
+    return ( 
+        <div className="wrapper">
+            <div className="join">
+
+                <h1 className="join__heading">Join</h1>
+
+                <div className="join__wrapper">
+                    <input className="join__wrapper-input" type="text" onChange={(e)=>setName(e.target.value)} placeholder="Name"/>
+                    <input className="join__wrapper-input" type="text" onChange={(e)=>setRoom(e.target.value)} placeholder="Room"/>
+                </div>
+
+                <button className="join__btn" type="submit" onClick={handleJoinClick}>Sign In</button>
+
+            </div>
+
+            {error?<h1 onLoad={()=>console.log('eloo')} ref={errorRef}>Uzupełnij dane!!!!</h1>:null}
+
+        </div>
+     );
+}
+ 
+export default Join;
