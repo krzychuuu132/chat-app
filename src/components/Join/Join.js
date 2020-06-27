146b24e4f85ebc4c9ab379aa616b664ea9f31e82
krzychuuu132 <52799848+krzychuuu132@users.noreply.github.com>
2020-06-28 00:28:26 +0200

    creating a join section/form validation/getting data V2

--- src/components/Join/Join.js
+++ src/components/Join/Join.js
@@ -1,5 +1,5 @@
 import React,{ useState ,useRef } from 'react';
-import { Link ,useHistory} from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import gsap from 'gsap'
 
 const Join = () => {
