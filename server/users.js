dc0f57319bc5a271f89fc413e2ccdcfe988b19e4
krzychuuu132 <52799848+krzychuuu132@users.noreply.github.com>
2020-07-02 01:14:33 +0200

    creating socket/socket-client(React)

---
+++ server/users.js
@@ -0,0 +1,51 @@
+
+
+const users = [];
+
+
+const addUser = ({  id, name, room }) => {
+    name = name.trim().toLowerCase();
+    room = room.trim().toLowerCase();
+
+    const exisitingUser = users.find(user=>user.name === name && user.room === room);
+
+    if(exisitingUser) {
+        return {error: 'Username is taken :('}
+    }
+
+    const user = { id, name, room };
+
+    users.push(user);
+
+    return { user };
+}
+
+
+
+const removeUser = (id) => {
+
+    const findUser  = users.findIndex(user=>user.id === id);
+
+    if(findUser === -1) {
+        return users.splice(findUser,1)[0];
+    }
+}
+
+
+
+const getUser = (id) => users.find(user=> user.id === id);
+
+
+
+const getUserInRoom = (room) => users.filter(user=> user.room === room);
+
+
+
+
+
+module.exports = {
+    addUser,
+    removeUser,
+    getUser,
+    getUserInRoom
+}
--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,23 +1,73 @@
-import  React,{useEffect}  from 'react';
+import  React,{ useEffect , useState }  from 'react';
 import  querystring from 'query-string';
 import io from 'socket.io-client'
 
+let socket;
+
 const Chat = ({ location }) => {
+    const ENDPOINT = 'localhost:5000';
+   
 
     const [ name,setName ] = useState('');
     const [ room,setRoom ] = useState('');
+    const [ messages, setMessages ] = useState([]);
+    const [ message, setMessage ] = useState('');
 
     useEffect(() => {
 
         const  { name , room }  = querystring.parse(location.search);
+
+         socket = io(ENDPOINT);
         
-        
+        socket.emit('join', { name , room },()=>{
+
+        });
+
         setName(name);
         setRoom(room);
-    })
+
+        return () =>{
+            socket.emit('disconnect');
+
+            socket.off();
+        }
+    },[ENDPOINT,location.search])
+
+    useEffect( ()=> {
+
+        socket.on('message', (message)=> {
+            console.log(messages+' elooo')
+
+            setMessages([...messages, message])
+
+        })
+
+
+    },[messages])
+
+    const sendMessage = (e) =>{
+       //e.preventDefault();
+       console.log(message)
+
+        if(message){
+            socket.emit('sendMessage' , message, ()=> sendMessage(''));
+        }
+    }
+
+    console.log(message,messages)
 
     return ( 
-        <h1>Chat!!!</h1>
+       <div>
+           <div>
+               <input 
+               value={message} 
+               onChange={(e)=>setMessage(e.target.value)} 
+               onKeyPress={ e => e.key === 'Enter'?sendMessage(e):null}
+             
+               
+               />
+           </div>
+       </div>
      );
 }
  
